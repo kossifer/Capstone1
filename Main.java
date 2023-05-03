11519eeabcb4dfa81d46f900e3eb8d97c9d91a28
kossifer <130480431+kossifer@users.noreply.github.com>
2023-05-03 15:22:16 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,63 @@
+package com.kg;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Home Screen");
+        System.out.println("\tD) Add Deposit");
+        System.out.println("\tP) Make Payment");
+        System.out.println("\tL) Ledger");
+        System.out.println("\tX) Exit");
+
+        System.out.println("Select an option");
+
+        boolean quit = false;
+        while (quit) {
+            System.out.println("Option: ");
+            String option = scanner.nextLine().toUpperCase();
+
+
+
+
+
+                    }
+            }
+
+
+
+
+
+        {
+
+
+
+            }
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
